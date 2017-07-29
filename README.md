f1d96b42e55d311053d2367b03823f4f17c3d1a2
vince <1061473663@qq.com>
2017-07-29 17:28:05 +0800

    实现项目框架搭建 git commit -m 实现项目框架搭建

---
+++ README.md
@@ -0,0 +1,21 @@
+# taobei
+
+> Taobei is an e-commerce website developed using vue2.0,including but not limited to vue-resource implementation of Ajax to obtain data information,express start data services,add routes to the project,use multiple components,etc.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/layout.vue
@@ -0,0 +1,136 @@
+<template>
+  <div>
+    <header class="app-head">
+      <sectiion class="app-head-inner">
+        <img src="../assets/logo.png" alt="icon">
+        <nav class="head-nav">
+          <ul class="nav-list">
+            <li>登录</li>
+            <li class="nav-pile">|</li>
+            <li>注册</li>
+            <li class="nav-pile">|</li>
+            <li>关于</li>
+          </ul>
+        </nav>
+      </sectiion>
+    </header>
+
+    <main class="container">content</main>
+
+    <footer class="app-foot">
+      <p>&copy; 2017 一丝墨</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  body {
+    background: #f0f2f5;
+    font-family: "Helvetica Neue",Helvetica,Arial,"Hiragino Sans GB","Hiragino Sans GB W3","Microsoft YaHei UI","Microsoft YaHei","WenQuanYi Micro Hei",sans-serif;
+    font-size: 14px;
+    color: #444;
+  }
+
+  .app-head{
+    background: #363636;
+    color: #b2b2b2;
+    width:100%;
+    height: 90px;
+    line-height: 90px;
+  }
+  .app-head-inner{
+    width: 1200px;
+    margin:0 auto;
+  }
+  .head-logo{
+    float: left;
+  }
+  .app-head-inner img{
+    width: 50px;
+    margin-top: 20px;
+  }
+  .head-nav{
+    float: right;
+  }
+  .head-nav ul{
+    overflow: hidden;
+  }
+  .head-nav li{
+    float: left;
+    cursor: pointer;
+  }
+  .nav-pile{
+    padding:0 10px;
+  }
+  .app-foot{
+    width:100%;
+    height: 80px;
+    line-height: 80px;
+    background: #e3e4e8;
+    text-align: center;
+    margin-top: 30px;
+    clear: both;
+  }
+  .container{
+    width: 1200px;
+    margin:0 auto;
+  }
+</style>
